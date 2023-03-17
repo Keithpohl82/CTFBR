20c2a6fab8b9ff7c5f4b3745e226c722773bd178
Keith Pohl <keith.pohl82@gmail.com>
2023-03-17 13:33:08 -0500

    Add project files.

---
+++ Source/CTFBR/CTFBRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CTFBRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CTFBR_API ACTFBRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
