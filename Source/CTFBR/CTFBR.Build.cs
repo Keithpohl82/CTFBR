246b772aae6aaa3a1d2c1b13575dae51997ba4b3
Keith Pohl <keith.pohl82@gmail.com>
2023-03-17 15:22:50 -0500

    Added 3rd person pack for enhanced input

--- Source/CTFBR/CTFBR.Build.cs
+++ Source/CTFBR/CTFBR.Build.cs
@@ -8,7 +8,7 @@ public class CTFBR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Niagara", "MultiplayerSessions", "OnlineSubsystem", "OnlineSubsystemSteam", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
